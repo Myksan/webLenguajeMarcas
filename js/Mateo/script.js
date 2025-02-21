c97afed0e81d864a9fd78ad0c0546f832b97ac70
AaronAyalaC <aayala@cifpfbmoll.eu>
2025-02-21 13:20:46 +0100

    Pantalla de carga en inicio

--- js/Mateo/script.js
+++ js/Mateo/script.js
@@ -1,1 +1,17 @@
+let dots = 0;
+function updateLoadingText() {
+    const loadingText = document.getElementById("loadingText");
+    loadingText.textContent = "Cargando" + ".".repeat(dots);
+    dots = (dots + 1) % 4;
+}
+const loadingInterval = setInterval(updateLoadingText, 400);
+
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        clearInterval(loadingInterval);
+        document.getElementById("loadingScreen").classList.add("hidden");
+        document.getElementById("content").style.visibility = "visible";
+    }, 8000);
+});
+
 
